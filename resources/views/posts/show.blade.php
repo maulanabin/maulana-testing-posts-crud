93c4cfd56cc4e54cacdacc314cd5d760fe884405
maulanabin <2041720132@student.polinema.ac.id>
2022-09-24 21:48:55 +0700

    Menambahkan Posts CRUD views

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Detail Post - Tutorial CRUD Laravel 8 @ qadrlabs.com</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- include summernote css -->
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="card border-0 shadow rounded">
+                    <div class="card-body">
+                        <form action="{{ route('post.update', $post->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <div class="card">
+                                    <div class="card-body">{{ $post->title }}</div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="status">Publish Status</label>
+                                <div class="card">
+                                    <div class="card-body">{{ $post->status == 1 ? 'Publish':'Draft' }}</div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="content">Content</label>
+                                <div class="card">
+                                    <div class="card-body">{{ $post->content }}</div>
+                                </div>
+                            </div>
+
+                            <a href="{{ route('post.edit', $post->id) }}" class="btn btn-md btn-primary">Edit Post</a>
+                            <a href="{{ route('post.index') }}" class="btn btn-md btn-secondary">back</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- include summernote js -->
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#content').summernote({
+                height: 250, //set editable area's height
+            });
+        })
+    </script>
+</body>
+
+</html>
